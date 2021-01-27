ffbab35fdbfb9cebc3f8b11b588cf5704b059395
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 15:16:56 +0700

    Create Using %timeit: formal name or literal syntax.py

---
+++ 2.Timing and profiling code/Using %timeit: formal name or literal syntax.py
@@ -0,0 +1,17 @@
+"""Create an empty list called formal_list using the formal name (list()).
+Create an empty list called literal_list using the literal syntax ([])."""
+# Create a list using the formal name
+formal_list = list()
+print(formal_list)
+
+# Create a list using the literal syntax
+literal_list = []
+print(literal_list)
+
+
+"""Print out the type of formal_list and literal_list to show that both naming conventions create a list."""
+# Print out the type of formal_list
+print(type(formal_list))
+
+# Print out the type of literal_list
+print(type(literal_list))
