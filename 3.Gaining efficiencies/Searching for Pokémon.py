76444712c66b3ca5a346994bc9134ae486b23cf9
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:23:09 +0700

    Update Searching for Pokémon.py

--- 3.Gaining efficiencies/Searching for Pokémon.py
+++ 3.Gaining efficiencies/Searching for Pokémon.py
@@ -1,10 +1,10 @@
-"""Convert Brock's Pokédex list (brock_pokedex) to a set called brock_pokedex_set."""
+# Convert Brock's Pokédex list (brock_pokedex) to a set called brock_pokedex_set.
 # Convert Brock's Pokédex to a set
 brock_pokedex_set = set(brock_pokedex)
 print(brock_pokedex_set)
 
 
-"""Check if 'Psyduck' is in Ash's Pokédex list (ash_pokedex) and if 'Psyduck' is in Brock's Pokédex set (brock_pokedex_set)."""
+# Check if 'Psyduck' is in Ash's Pokédex list (ash_pokedex) and if 'Psyduck' is in Brock's Pokédex set (brock_pokedex_set).
 # Convert Brock's Pokédex to a set
 brock_pokedex_set = set(brock_pokedex)
 print(brock_pokedex_set)
@@ -14,7 +14,7 @@ print('Psyduck' in ash_pokedex)
 print('Psyduck' in brock_pokedex_set)
 
 
-"""Check if 'Machop' is in Ash's Pokédex list (ash_pokedex) and if 'Machop' is in Brock's Pokédex set (brock_pokedex_set)."""
+# Check if 'Machop' is in Ash's Pokédex list (ash_pokedex) and if 'Machop' is in Brock's Pokédex set (brock_pokedex_set).
 # Convert Brock's Pokédex to a set
 brock_pokedex_set = set(brock_pokedex)
 print(brock_pokedex_set)
