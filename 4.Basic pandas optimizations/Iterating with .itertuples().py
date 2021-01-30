3eccbd42372142186bf8ffa0d40ddb4b294871d5
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 18:52:48 +0700

    Update Iterating with .itertuples().py

--- 4.Basic pandas optimizations/Iterating with .itertuples().py
+++ 4.Basic pandas optimizations/Iterating with .itertuples().py
@@ -1,10 +1,10 @@
-"""Use .itertuples() to loop over rangers_df and print each row."""
+# --Use .itertuples() to loop over rangers_df and print each row.--
 # Loop over the DataFrame and print each row
 for row in rangers_df.itertuples():
   print(row)
   
 
-"""Loop over rangers_df with .itertuples() and save each row's Index, Year, and Wins (W) attribute as i, year, and wins."""
+# --Loop over rangers_df with .itertuples() and save each row's Index, Year, and Wins (W) attribute as i, year, and wins.--
 # Loop over the DataFrame and print each row's Index, Year and Wins (W)
 for row in rangers_df.itertuples():
   i = row.Index
@@ -13,7 +13,7 @@ for row in rangers_df.itertuples():
   print(i, year, wins)
   
 
-""Now, loop over rangers_df and print these values only for those rows where the Rangers made the playoffs."""
+# --Now, loop over rangers_df and print these values only for those rows where the Rangers made the playoffs.--
   # Check if rangers made Playoffs (1 means yes; 0 means no)
   if row.Playoffs == 1:
     print(i, year, wins)
