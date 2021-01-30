9a1ea8a963c6f01b94fe481ad07610ec74b8f239
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:18:13 +0700

    Update Bringing it all together: Festivus!.py

--- 1.Foundations for efficiencies/Bringing it all together: Festivus!.py
+++ 1.Foundations for efficiencies/Bringing it all together: Festivus!.py
@@ -1,12 +1,12 @@
-"""Use range() to create a list of arrival times (10 through 50 incremented by 10). Create the list arrival_times by unpacking the range object."""
+# --Use range() to create a list of arrival times (10 through 50 incremented by 10). Create the list arrival_times by unpacking the range object.--
 # Create a list of arrival times
 arrival_times = [*range(10, 60, 10)]
 
 print(arrival_times)
 
 
-"""You realize your clock is three minutes fast. 
-Convert the arrival_times list into a numpy array (called arrival_times_np) and use NumPy broadcasting to subtract three minutes from each arrival time."""
+# --You realize your clock is three minutes fast. 
+# Convert the arrival_times list into a numpy array (called arrival_times_np) and use NumPy broadcasting to subtract three minutes from each arrival time.--
 # Create a list of arrival times
 arrival_times = [*range(10,60,10)]
 
@@ -17,8 +17,8 @@ new_times = arrival_times_np - 3
 print(new_times)
 
 
-"""Use list comprehension with enumerate() to pair each guest in the names list to their updated arrival time in the new_times array. 
-You'll need to use the index variable created from using enumerate() on new_times to index the names list."""
+# --Use list comprehension with enumerate() to pair each guest in the names list to their updated arrival time in the new_times array. 
+# You'll need to use the index variable created from using enumerate() on new_times to index the names list.--
 # Create a list of arrival times
 arrival_times = [*range(10,60,10)]
 
@@ -32,8 +32,8 @@ guest_arrivals = [(names[a],time) for a,time in enumerate(new_times)]
 print(guest_arrivals)
 
 
-"""A function named welcome_guest() has been pre-loaded into your session. 
-Use map() to apply this function to each element of the guest_arrivals list and save it as the variable welcome_map."""
+# --A function named welcome_guest() has been pre-loaded into your session. 
+# Use map() to apply this function to each element of the guest_arrivals list and save it as the variable welcome_map.--
 # Create a list of arrival times
 arrival_times = [*range(10,60,10)]
 
