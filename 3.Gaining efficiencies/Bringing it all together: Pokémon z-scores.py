984327f4e0957b27a2fde77deff479ceb11420bb
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:20:35 +0700

    Update Bringing it all together: Pokémon z-scores.py

--- 3.Gaining efficiencies/Bringing it all together: Pokémon z-scores.py
+++ 3.Gaining efficiencies/Bringing it all together: Pokémon z-scores.py
@@ -1,5 +1,5 @@
-"""Use NumPy to eliminate the for loop used to create the z-scores.
-Then, combine the names, hps, and z_scores objects together into a list called poke_zscores2."""
+# --Use NumPy to eliminate the for loop used to create the z-scores.
+# Then, combine the names, hps, and z_scores objects together into a list called poke_zscores2.--
 # Calculate the total HP avg and total HP standard deviation
 hp_avg = hps.mean()
 hp_std = hps.std()
@@ -11,7 +11,7 @@ z_scores = (hps - hp_avg)/hp_std
 poke_zscores2 = [*zip(names, hps, z_scores)]
 print(*poke_zscores2[:3], sep='\n')
 
-"""Use list comprehension to replace the for loop used to collect Pokémon with the highest HPs based on their z-score."""
+# --Use list comprehension to replace the for loop used to collect Pokémon with the highest HPs based on their z-score.--
 # Use list comprehension with the same logic as the highest_hp_pokemon code block
 highest_hp_pokemon2 =  [(name, hp, zscore) for name,hp,zscore in poke_zscores2 if zscore > 2]
 print(*highest_hp_pokemon2, sep='\n')
