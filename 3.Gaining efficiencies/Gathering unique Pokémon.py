502f94521ef88dd2d25429cb58c4364a49fd6d7b
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:22:19 +0700

    Update Gathering unique Pokémon.py

--- 3.Gaining efficiencies/Gathering unique Pokémon.py
+++ 3.Gaining efficiencies/Gathering unique Pokémon.py
@@ -1,10 +1,10 @@
-"""Use the provided function to collect the unique Pokémon in the names list. Save this as uniq_names_func."""
+# --Use the provided function to collect the unique Pokémon in the names list. Save this as uniq_names_func.--
 # Use the provided function to collect unique Pokémon names
 uniq_names_func = find_unique_items(names)
 print(len(uniq_names_func))
 
 
-"""Use a set data type to collect the unique Pokémon in the names list. Save this as uniq_names_set."""
+# --Use a set data type to collect the unique Pokémon in the names list. Save this as uniq_names_set.--
 # Convert the names list to a set to collect unique Pokémon names
 uniq_names_set = set(names)
 print(len(uniq_names_set))
@@ -13,8 +13,8 @@ print(len(uniq_names_set))
 print(sorted(uniq_names_func) == sorted(uniq_names_set))
 
 
-"""Use the most efficient approach for gathering unique items to collect the unique Pokémon types (from the primary_types list) 
-and Pokémon generations (from the generations list)."""
+# --Use the most efficient approach for gathering unique items to collect the unique Pokémon types (from the primary_types list) 
+# and Pokémon generations (from the generations list).--
 # Use the best approach to collect unique primary types and generations
 uniq_types = set(primary_types)
 uniq_gens = set(generations)
