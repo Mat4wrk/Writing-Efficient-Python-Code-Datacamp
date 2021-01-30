26c7599f50d43da1e6cf44f5459a15ae6d40be9b
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 19:00:14 +0700

    Update Run differentials with .iterrows().py

--- 4.Basic pandas optimizations/Run differentials with .iterrows().py
+++ 4.Basic pandas optimizations/Run differentials with .iterrows().py
@@ -1,16 +1,16 @@
-"""Create an empty list called run_diffs that will be used to store the run differentials you will calculate."""
+# --Create an empty list called run_diffs that will be used to store the run differentials you will calculate.--
 # Create an empty list to store run differentials
 run_diffs = []
 
 
-"""Write a for loop that uses .iterrows() to loop over giants_df and collects each row's runs scored and runs allowed."""
+# --Write a for loop that uses .iterrows() to loop over giants_df and collects each row's runs scored and runs allowed.--
 # Write a for loop and collect runs allowed and runs scored for each row
 for i,row in giants_df.iterrows():
     runs_scored = row[3]
     runs_allowed = row[[4]]
     
     
- """Add a line to the for loop that uses the provided function to calculate each row's run differential"""
+ #  --Add a line to the for loop that uses the provided function to calculate each row's run differential--
  # Write a for loop and collect runs allowed and runs scored for each row
 for i,row in giants_df.iterrows():
     runs_scored = row['RS']
@@ -20,7 +20,7 @@ for i,row in giants_df.iterrows():
     run_diff = calc_run_diff(runs_scored, runs_allowed)
    
    
-"""Add a line to the loop that appends each row's run differential to the run_diffs list."""
+# --Add a line to the loop that appends each row's run differential to the run_diffs list.--
     # Append each run differential to the output list
     run_diffs.append(run_diff)
 
