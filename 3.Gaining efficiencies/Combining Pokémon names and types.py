f768bdffe3e280ab9bba1452d0189db02733fbde
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:21:26 +0700

    Update Combining Pokémon names and types.py

--- 3.Gaining efficiencies/Combining Pokémon names and types.py
+++ 3.Gaining efficiencies/Combining Pokémon names and types.py
@@ -1,18 +1,18 @@
-"""Combine the names list and the primary_types list into a new list object (called names_type1)."""
+# --Combine the names list and the primary_types list into a new list object (called names_type1).--
 # Combine names and primary_types
 names_type1 = [*zip(names, primary_types)]
 
 print(*names_type1[:5], sep='\n')
 
 
-"""Combine names, primary_types, and secondary_types (in that order) using zip() and unpack the zip object into a new list."""
+# --Combine names, primary_types, and secondary_types (in that order) using zip() and unpack the zip object into a new list.--
 # Combine all three lists together
 names_types = [*zip(names, primary_types, secondary_types)]
 
 print(*names_types[:5], sep='\n')
 
 
-"""Use zip() to combine the first five items from the names list and the first three items from the primary_types list."""
+# --Use zip() to combine the first five items from the names list and the first three items from the primary_types list.--
 # Combine five items from names and three items from primary_types
 differing_lengths = [*zip(names[:5], primary_types[:3])]
 
